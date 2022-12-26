34603751afbdce03046262db97c93404fabd17ed
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2022-12-26 18:23:08 +0530

    Changes in authorization logic

--- app/Routes/tenantRoutes.js
+++ app/Routes/tenantRoutes.js
@@ -1,16 +1,23 @@
 import express from "express";
+import { ROLES } from "../../Constants.js";
 import tenantController from "../Controllers/tenantController.js";
 import { isAuthenticated } from "../middleWares/authentication.js";
-import { IS_SUPER_ADMIN } from "../middleWares/authorization.js";
+import { CAPABILITY } from "../middleWares/authorization.js";
 
 const authRouter = express.Router();
 authRouter.use(isAuthenticated);
-authRouter.use(IS_SUPER_ADMIN);
+
 authRouter
   .route("/")
-  .get(tenantController.getTenant)
-  .post(tenantController.createTenant)
-  .put(tenantController.updateTenant);
+  .post(CAPABILITY([ROLES.SUPER_ADMIN.VALUE]), tenantController.createTenant)
+  .put(CAPABILITY([ROLES.SUPER_ADMIN.VALUE]), tenantController.updateTenant);
 
 authRouter.route("/:id").delete(tenantController.deleteTenant);
+
+authRouter
+  .route("/")
+  .get(
+    CAPABILITY([ROLES.SUPER_ADMIN.VALUE, ROLES.ADMIN.VALUE]),
+    tenantController.getTenant
+  );
 export default authRouter;
--- app/middleWares/authorization.js
+++ app/middleWares/authorization.js
@@ -4,65 +4,30 @@ import { ROLES, RESPONSE_STATUS, STATUS } from "../../Constants.js";
 
 env.config();
 
-export const IS_SUPER_ADMIN = (req, res, next) => {
-  try {
-    let token = req.header("Authorization");
-    token = token.replace("Bearer ", "");
-    // eslint-disable-next-line no-undef
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded.role === ROLES.SUPER_ADMIN.VALUE) {
-      next();
-    } else {
-      throw Error("Unauthorized to access this page.");
+export const CAPABILITY =
+  (roles = []) =>
+  (req, res, next) => {
+    if (roles.length === 0) {
+      roles = [ROLES.SUPER_ADMIN.VALUE, ROLES.ADMIN.VALUE, ROLES.USER.VALUE];
     }
-  } catch (e) {
-    removeCookies(res);
-    console.log({ message: `UNAUTHORIZED ACCESS!!` });
-    res
-      .status(RESPONSE_STATUS.FORBIDDEN_403)
-      .send({ message: `UNAUTHORIZED ACCESS!!`, status: STATUS.FAILURE });
-  }
-};
-
-export const IS_ADMIN = (req, res, next) => {
-  try {
-    let token = req.header("Authorization");
-    token = token.replace("Bearer ", "");
-    // eslint-disable-next-line no-undef
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded.role === ROLES.ADMIN.VALUE) {
-      next();
-    } else {
-      throw Error("Unauthorized to access this page.");
+    try {
+      let token = req.header("Authorization");
+      token = token.replace("Bearer ", "");
+      // eslint-disable-next-line no-undef
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      if (roles.includes(decoded.role)) {
+        next();
+      } else {
+        throw Error("Unauthorized to access this page.");
+      }
+    } catch (e) {
+      removeCookies(res);
+      console.log({ message: `UNAUTHORIZED ACCESS!!` });
+      res
+        .status(RESPONSE_STATUS.FORBIDDEN_403)
+        .send({ message: `UNAUTHORIZED ACCESS!!`, status: STATUS.FAILURE });
     }
-  } catch (e) {
-    removeCookies(res);
-    console.log({ message: `UNAUTHORIZED ACCESS!!` });
-    res
-      .status(RESPONSE_STATUS.FORBIDDEN_403)
-      .send({ message: `UNAUTHORIZED ACCESS!!`, status: STATUS.FAILURE });
-  }
-};
-
-export const IS_USER = (req, res, next) => {
-  try {
-    let token = req.header("Authorization");
-    token = token.replace("Bearer ", "");
-    // eslint-disable-next-line no-undef
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded.role === ROLES.USER.VALUE) {
-      next();
-    } else {
-      throw Error("Unauthorized to access this page.");
-    }
-  } catch (e) {
-    removeCookies(res);
-    console.log({ message: `UNAUTHORIZED ACCESS!!` });
-    res
-      .status(RESPONSE_STATUS.FORBIDDEN_403)
-      .send({ message: `UNAUTHORIZED ACCESS!!`, status: STATUS.FAILURE });
-  }
-};
+  };
 
 export const removeCookies = (res) => {
   res.clearCookie("sid", { path: "/" });
